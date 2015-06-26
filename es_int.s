1ad2f209255dbd2504423d35a727102df663b3ea
David Páez <davidpaezalderete@gmail.com>
2015-06-26 19:34:59 +0200

    testing

--- es_int.s
+++ es_int.s
@@ -7,7 +7,7 @@
 
 	ORG	$0
 	DC.L	$8000		* Inicio de Pila
-	DC.L	PR39		* PC al inicio de PPAL1
+	DC.L	PR42		* PC al inicio de PPAL1
 
 ********* Definición de los registros *********************
 
@@ -853,3 +853,42 @@ PR39:
     BREAK
 
 
+PR42:
+    BSR INIT
+    MOVE.L pSB,A1
+    MOVE.L pSB,A2
+    * MOVE.L		#$FFFFFFFF,D0
+    MOVE.L #190,D2
+    MOVE.L #0,D3
+PR42_BUC:
+    ADD.L  #1,D3
+    MOVE.L #1,D1
+	MOVE.B D1,(A1)+ 
+	MOVE.L #2,D1
+	MOVE.B D1,(A1)+	
+	MOVE.L #3,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #4,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #5,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #6,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #7,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #8,D1
+	MOVE.B D1,(A1)+
+	MOVE.L #9,D1	
+	MOVE.B D1,(A1)+
+	MOVE.L #0,D1	
+	MOVE.B D1,(A1)+
+    CMP.L  D3,D2
+    BGT    PR42_BUC
+	MOVE.L #$0D,D1
+	MOVE.B D1,(A1)+ 
+	MOVE.L A1,pSB
+    MOVE.W #10,-(A7) * Tama~no de escritura
+	MOVE.W #DESA,-(A7) * Puerto B
+	MOVE.L A2,-(A7) * Direcci ́on de lectura
+    BSR PRINT
+    BREAK
